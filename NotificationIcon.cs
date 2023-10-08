2d2de9941a6929deb33cf1b123f335ef253aba42
YorVeX <528974+YorVeX@users.noreply.github.com>
2023-10-08 06:09:51 +0200

    Initial checkin of a fully working version

---
+++ NotificationIcon.cs
@@ -0,0 +1,180 @@
+﻿// SPDX-FileCopyrightText: © 2019-2023 YorVeX, https://github.com/YorVeX
+// SPDX-License-Identifier: MIT
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace xComboText
+{
+  public sealed class NotificationIcon
+  {
+    private readonly NotifyIcon _notifyIcon;
+    private readonly ContextMenu notificationMenu;
+    readonly List<FileSystemWatcher> _watchers = new List<FileSystemWatcher>();
+    readonly Settings _settings = new Settings();
+    readonly object _fileWriteLock = new object();
+
+    #region Initialize icon and menu
+    public NotificationIcon()
+    {
+      _notifyIcon = new NotifyIcon();
+      notificationMenu = new ContextMenu(InitializeMenu());
+
+      _notifyIcon.DoubleClick += IconDoubleClick;
+      _notifyIcon.Icon = (Icon)new System.ComponentModel.ComponentResourceManager(typeof(NotificationIcon)).GetObject("$this.Icon");
+      _notifyIcon.ContextMenu = notificationMenu;
+
+      _settings.LoadFromFile(Application.ProductName + ".conf");
+
+      foreach (var fileCombo in _settings.FileCombos.FileComboList)
+      {
+        combineFileTexts(fileCombo);
+        foreach (var inputFile in fileCombo.InputFiles)
+        {
+          try
+          {
+            FileSystemWatcher watcher = new FileSystemWatcher(Path.GetDirectoryName(inputFile.InputFile), Path.GetFileName(inputFile.InputFile));
+            watcher.Changed += (sender, e) => combineFileTexts(fileCombo);
+            watcher.EnableRaisingEvents = true;
+            _watchers.Add(watcher);
+          }
+          catch (Exception ex)
+          {
+            MessageBox.Show(ex.GetType().Name + " creating file watcher for " + inputFile.InputFile + ":\n" + ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+          }
+        }
+      }
+    }
+
+    private MenuItem[] InitializeMenu()
+    {
+      MenuItem[] menu = new MenuItem[] {
+        new MenuItem("About", menuAboutClick),
+        new MenuItem("Exit", menuExitClick)
+      };
+      return menu;
+    }
+    #endregion
+
+    #region Main - Program entry point
+    /// <summary>Program entry point.</summary>
+    [STAThread]
+    public static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+
+      // Please use a unique name for the mutex to prevent conflicts with other programs
+      using (Mutex mtx = new Mutex(true, "xComboText", out bool isFirstInstance))
+      {
+        if (isFirstInstance)
+        {
+          NotificationIcon notificationIcon = new NotificationIcon();
+          notificationIcon._notifyIcon.Visible = true;
+          Application.Run();
+          notificationIcon._notifyIcon.Dispose();
+        }
+        else
+        {
+          // The application is already running
+          // TODO: Display message box or change focus to existing application instance
+        }
+      } // releases the Mutex
+    }
+    #endregion
+
+    #region private methods
+    private void showBalloonInvoke(int timeout, string tipTitle, string tipText, ToolTipIcon tipIcon)
+    {
+      // ShowBalloonTip() runs on the GUI thread so it needs to be invoked if called from a different thread
+      System.Reflection.MethodInfo methodInfo = typeof(NotifyIcon).GetMethod("ShowBalloonTip", new[] { typeof(int), typeof(string), typeof(string), typeof(ToolTipIcon) });
+      methodInfo.Invoke(_notifyIcon, new object[] { timeout, tipText, tipTitle, tipIcon });
+    }
+
+    private void combineFileTexts(FileCombo fileCombo)
+    {
+      Debug.WriteLine(string.Format("[{0:T}] Writing combined file.", DateTime.Now));
+      lock (_fileWriteLock)
+      {
+        string comboString = "";
+        foreach (var inputFile in fileCombo.InputFiles)
+        {
+          string fileContent = "";
+          try
+          {
+            if (File.Exists(inputFile.InputFile))
+              fileContent = File.ReadAllText(inputFile.InputFile);
+          }
+          catch (IOException)
+          {
+            // retry a bit later
+            (new Thread(() =>
+            {
+              Thread.Sleep(3000);
+              Debug.WriteLine("File read retry after IOException.");
+              combineFileTexts(fileCombo);
+            })).Start();
+            return;
+          }
+          catch (Exception ex)
+          {
+            showBalloonInvoke(5000, Application.ProductName, ex.GetType().Name + " trying to read " + inputFile.InputFile, ToolTipIcon.Error);
+          }
+
+          if (string.IsNullOrEmpty(fileContent))
+          {
+            if (inputFile.SkipIfEmpty)
+              continue;
+            fileContent = inputFile.EmptyText;
+          }
+          if (!string.IsNullOrEmpty(inputFile.ReplaceSearch) && !string.IsNullOrEmpty(inputFile.ReplaceWith))
+            fileContent = fileContent.Replace(inputFile.ReplaceSearch, inputFile.ReplaceWith);
+          comboString += (comboString == "" ? "" : fileCombo.Separator) + inputFile.Prefix + fileContent + inputFile.Suffix;
+        }
+        try
+        {
+          if (string.IsNullOrEmpty(comboString))
+            File.WriteAllText(fileCombo.OutputFile, fileCombo.EmptyText);
+          else
+            File.WriteAllText(fileCombo.OutputFile, fileCombo.Prefix + comboString + fileCombo.Suffix);
+        }
+        catch (Exception ex)
+        {
+          showBalloonInvoke(5000, Application.ProductName, ex.GetType().Name + " trying to write " + fileCombo.OutputFile, ToolTipIcon.Error);
+        }
+      }
+    }
+
+    #endregion private methods
+
+    #region Event Handlers
+    private void menuAboutClick(object sender, EventArgs e)
+    {
+      MessageBox.Show(Application.ProductName + " v" + Application.ProductVersion);
+    }
+
+    private void menuExitClick(object sender, EventArgs e)
+    {
+      foreach (FileSystemWatcher watcher in _watchers)
+        watcher.Dispose();
+      Application.Exit();
+    }
+
+    private void IconDoubleClick(object sender, EventArgs e)
+    {
+      //			MessageBox.Show("The icon was double clicked");
+
+      //			var list = new List<FileComboInputFile>();
+      //			list.Add(new FileComboInputFile("input1.txt", "Input 1:", ""));
+      //			list.Add(new FileComboInputFile("input2.txt", "Input 2:", ""));
+      //			_settings.FileCombos.FileComboList.Add(new FileCombo("output.txt", ", ", list));
+      //			_settings.SaveToFile("Example.conf");
+    }
+    #endregion
+  }
+}
---
+++ Settings.cs
@@ -0,0 +1,124 @@
+﻿// SPDX-FileCopyrightText: © 2019-2023 YorVeX, https://github.com/YorVeX
+// SPDX-License-Identifier: MIT
+
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace xComboText
+{
+  public struct FileComboInputFile
+  {
+    public string InputFile { get; set; }
+    public string Prefix { get; set; }
+    public string Suffix { get; set; }
+    public string ReplaceSearch { get; set; }
+    public string ReplaceWith { get; set; }
+    public string EmptyText { get; set; }
+    public bool SkipIfEmpty { get; set; }
+  }
+
+  public class FileCombo
+  {
+    public string OutputFile { get; set; }
+    public string Prefix { get; set; }
+    public string Suffix { get; set; }
+    public string Separator { get; set; }
+    public string EmptyText { get; set; }
+    [XmlArray]
+    [XmlArrayItem(ElementName = "InputFile")]
+    public List<FileComboInputFile> InputFiles { get; set; }
+
+    public FileCombo()
+    {
+      Separator = ", ";
+    }
+
+    public FileCombo(string outputFile, string prefix, string suffix, string separator, string emptyText, List<FileComboInputFile> inputFiles)
+    {
+      OutputFile = outputFile;
+      Prefix = prefix;
+      Suffix = suffix;
+      Separator = separator;
+      EmptyText = emptyText;
+      InputFiles = inputFiles;
+    }
+  }
+
+  public class FileCombosSettings
+  {
+    [XmlArray]
+    [XmlArrayItem(ElementName = "FileCombo")]
+    public List<FileCombo> FileComboList { get; set; }
+
+    // the XML serializer always uses this to load from settings, so here's where default values are set
+    public FileCombosSettings()
+    {
+    }
+  }
+
+  public class MiscSettings
+  {
+    public string Dummy { get; set; }
+
+    // the XML serializer always uses this to load from settings, so here's where default values are set
+    public MiscSettings()
+    {
+      Dummy = "";
+    }
+  }
+
+  public class Configuration
+  {
+    public Settings Settings { get; set; }
+
+    public Configuration()
+    {
+      Settings = new Settings();
+    }
+
+    public Configuration(Settings settings)
+    {
+      Settings = settings;
+    }
+  }
+
+  /// <summary>
+  /// Description of Settings.
+  /// </summary>
+  public class Settings
+  {
+    public FileCombosSettings FileCombos { get; set; }
+    public MiscSettings Misc { get; set; }
+
+    public Settings()
+    {
+      FileCombos = new FileCombosSettings();
+      Misc = new MiscSettings();
+    }
+
+    public void LoadFromFile(string fileName)
+    {
+      if (!File.Exists(fileName))
+        return;
+      Configuration loadConfig = new Configuration(this);
+      using (TextReader textReader = new StreamReader(fileName))
+        loadConfig = (Configuration)new XmlSerializer(loadConfig.GetType()).Deserialize(textReader);
+
+      FileCombos = loadConfig.Settings.FileCombos;
+      Misc = loadConfig.Settings.Misc;
+    }
+
+    public void SaveToFile(string fileName)
+    {
+      Configuration config = new Configuration(this);
+      using (XmlTextWriter xmlWriter = new XmlTextWriter(fileName, System.Text.Encoding.UTF8))
+      {
+        xmlWriter.Formatting = Formatting.Indented;
+        new XmlSerializer(config.GetType()).Serialize(xmlWriter, config);
+      }
+    }
+  }
+}
+
